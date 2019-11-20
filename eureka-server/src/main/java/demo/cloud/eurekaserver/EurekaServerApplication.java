48fd120047bb56fe2a7cc04f0a956a46cdcae399
dean.lee <dean.lee@aliyun.com>
2019-11-20 19:20:31 +0800

    eureka server

---
+++ eureka-server/src/main/java/demo/cloud/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package demo.cloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author dean.lee
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
